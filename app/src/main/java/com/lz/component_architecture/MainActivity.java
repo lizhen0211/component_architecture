28a5eedcdb9fc6aab7e9e3f785dfa6ca7bf6fe4c
lizhen <lizhen0211@163.com>
2019-07-03 14:06:03 +0800

    初始化工程

---
+++ app/src/main/java/com/lz/component_architecture/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lz.component_architecture;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author lizhen
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
